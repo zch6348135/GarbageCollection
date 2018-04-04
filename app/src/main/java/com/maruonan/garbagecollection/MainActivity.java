975e8fabd03c1668dc02859ac3ab06ca7812f378
zch <353756645@qq.com>
2018-04-04 09:48:03 +0800

    beta_0.0.3

--- app/src/main/java/com/maruonan/garbagecollection/MainActivity.java
+++ app/src/main/java/com/maruonan/garbagecollection/MainActivity.java
@@ -1,7 +1,10 @@
 package com.maruonan.garbagecollection;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,35 +14,43 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.maruonan.garbagecollection.ui.AppointmentFragment;
+import com.maruonan.garbagecollection.ui.InfoFragment;
+import com.maruonan.garbagecollection.ui.PointFragment;
 import com.maruonan.garbagecollection.ui.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout mDrawer;
+    private Fragment currentFragment;     //记录当前正在使用的fragment
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+        mDrawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView =  findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        initFragment(savedInstanceState);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            MainActivity.this.finish();
+            //super.onBackPressed();
         }
     }
 
@@ -52,46 +63,58 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            toolbar.setTitle("设置");
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.replace(R.id.container, new SettingsFragment()).commitAllowingStateLoss();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        switch (item.getItemId()) {
 
-        if (id == R.id.nav_appointment) {
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.replace(R.id.container, new SettingsFragment()) .commitAllowingStateLoss();
-            // Handle the camera action
-        } else if (id == R.id.nav_point) {
+            case R.id.nav_appointment:
+                toolbar.setTitle("我的预约");
+                currentFragment = new AppointmentFragment();
+                break;
+            case R.id.nav_point:
+                toolbar.setTitle("我的积分");
+                currentFragment = new PointFragment();
 
-        }else if (id == R.id.nav_info) {
-
+                break;
+            case R.id.nav_info:
+                toolbar.setTitle("个人信息");
+                currentFragment = new InfoFragment();
+                break;
         }
-
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-
+        fragmentTransaction.replace(R.id.container, currentFragment).commitAllowingStateLoss();
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    /**
+     * 为页面加载初始状态的fragment
+     */
+    public void initFragment(Bundle savedInstanceState)
+    {
+        //判断activity是否重建，如果不是，则不需要重新建立fragment.
+        if(savedInstanceState==null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.addToBackStack(null);
+            if(currentFragment==null) {
+                toolbar.setTitle("我的预约");
+                currentFragment = new AppointmentFragment();
+            }
+            fragmentTransaction.replace(R.id.container, currentFragment).commitAllowingStateLoss();
+        }
     }
 }
